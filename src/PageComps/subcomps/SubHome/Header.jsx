488ea838c34cab7c19232a1934b505c308805581
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-14 00:17:17 +0500

    Header of home page

---
+++ src/PageComps/subcomps/SubHome/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <img src="public\images\website_6.webp " alt="" />
+      <button className=" text-white  border-white border  duration-500 ease-in hover:bg-black  relative text-1xl font-light  w-38 h-10 bottom-20  left-138  text-center  bg-transparent ">
+        SHOP NOW
+      </button>
+    </>
+  );
+}
+
+export default Header;
