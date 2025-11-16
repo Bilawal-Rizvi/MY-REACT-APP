235435e4299f03bfa6a3bf69ad54a5e524272b70
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-17 01:43:03 +0500

    ADDED STITCHED PAGE Components

---
+++ src/PageComps/subcomps/SubStitches/Topstit.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Topstit() {
+  return (
+    <>
+      <div className="  flex flex-col justify-center mt-20 h-60 items-center gap-20">
+        <h1 className="text-4xl">UNSTITCHED</h1>
+        <ul className="flex gap-8 text-gray-500  ">
+          <li className="ul relative">FORMAL</li>
+          <li className="ul relative">SUMMER</li>
+          <li className="ul relative">VELVET</li>
+          <li className="ul relative">WINTER</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Topstit;
