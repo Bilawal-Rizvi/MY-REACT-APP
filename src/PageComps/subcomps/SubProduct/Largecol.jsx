baec320295e68c950019f5420b409f63bcdbc3fc
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-18 02:09:12 +0500

    Product page Components

---
+++ src/PageComps/subcomps/SubProduct/Largecol.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Largeimg from "./Largeimg";
+function Largecol({img}) {
+  return (
+    <>
+      <Largeimg url={img} />
+    </>
+  );
+}
+
+export default Largecol;
