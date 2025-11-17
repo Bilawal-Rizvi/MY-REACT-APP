fb2a20ea44c3ebe105b2f2dbd35e4c8cbf64424e
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-18 02:11:25 +0500

    Product page Components

---
+++ src/PageComps/subcomps/SubProduct/Topdel.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Topdel() {
+  return (
+    <>
+      <div className="flex flex-col gap-3 border-b pb-5 ">
+        <h1 className="text-gray-400">EMBROIDERED VISCOSE SILK UF-4306</h1>
+        <b>PKR 6,950.00</b>
+        <span className="text-gray-400 s">EWUF4306 ● 3 PIECE</span>
+      </div>
+    </>
+  );
+}
+
+export default Topdel;
