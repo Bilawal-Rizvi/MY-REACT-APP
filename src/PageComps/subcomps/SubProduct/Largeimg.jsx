8267e7c0812032790173f510e0865fe6295873a2
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-18 02:09:34 +0500

    Product page Components

---
+++ src/PageComps/subcomps/SubProduct/Largeimg.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Largeimg(props) {
+  return (
+    <>
+      <div>
+        <img src={props.url} width="533.5" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default Largeimg;
