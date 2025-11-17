065fc5f0e461a7ba4a5eef6370ce8c6deb414763
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-18 02:11:06 +0500

    Product page Components

---
+++ src/PageComps/subcomps/SubProduct/Smallimg.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Smallimg(props) {
+  return (
+    <>
+      <div>
+      
+        <img src={props.URL} width="55" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default Smallimg;
