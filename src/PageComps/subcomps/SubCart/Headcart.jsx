b14c7d5922c3b4a0a0eb55ae5952025d78d72ae3
Bilawal-Rizvi <shahb7404@gmail.com>
2025-11-20 01:11:15 +0500

    CART PAGE Components

---
+++ src/PageComps/subcomps/SubCart/Headcart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Headcart() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center mb-20">
+        <h1 className="text-center text-4xl mb-4 ">CART</h1>
+        <span className="font-light text-gray-400">
+          You are eligible for free shipping.
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default Headcart;
